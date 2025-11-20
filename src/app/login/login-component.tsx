0d3cd41b69aaab88942a930d832820d00705444c
AndDev <23031429@itcelaya.edu.mx>
2025-11-20 15:01:21 -0600

    feat: update login component with user state management and remove unused signout route

--- src/app/login/login-component.tsx
+++ src/app/login/login-component.tsx
@@ -1,29 +1,54 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { loginWithGoogle } from "./action";
+import { createClient } from "@/utils/supabase/client";
+import { User } from "@supabase/supabase-js";
 
 const LoginComponent = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const supabase = createClient();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      setUser(user);
+    };
+
+    fetchUser();
+  }, [supabase]);
+
   const handleLogin = async () => {
     await loginWithGoogle();
   };
 
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+    setUser(null);
+  };
+
   return (
     <div>
-      <button
-        onClick={handleLogin}
-        className="px-4 py-2 bg-blue-500 text-white rounded"
-      >
-        Login with Google
-      </button>
-
-      <form action="/auth/signout" method="post">
+      {user ? (
+        <div>
+          <span className="mr-4">Hello, {user.email}</span>
+          <button
+            onClick={handleLogout}
+            type="submit"
+            className="px-4 py-2 bg-red-500 text-white rounded ml-4"
+          >
+            Logout
+          </button>
+        </div>
+      ) : (
         <button
-          type="submit"
-          className="px-4 py-2 bg-red-500 text-white rounded ml-4"
+          onClick={handleLogin}
+          className="px-4 py-2 bg-blue-500 text-white rounded"
         >
-          Logout
+          Login with Google
         </button>
-      </form>
+      )}
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,11 @@
-import { createClient } from "@/utils/supabase/server";
 import LoginComponent from "./login/login-component";
 
 export default async function Home() {
-  const supabase = await createClient();
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-4xl font-bold mb-8">
-        Welcome to Supabase with Next.js!
+        Nextjs 16 + Supabase OAuth (Google)
       </h1>
-
-      <p>{user ? user.email : ""}</p>
 
       <LoginComponent />
     </div>
